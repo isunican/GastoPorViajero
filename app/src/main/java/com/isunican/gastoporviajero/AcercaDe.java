50bebe9da60409e456e2bd7240231e3b30336285
juanmrivas <jmrivasconcepcion@gmail.com>
2020-09-26 12:49:55 +0200

    target API 29 including androidx

--- app/src/main/java/com/isunican/gastoporviajero/AcercaDe.java
+++ app/src/main/java/com/isunican/gastoporviajero/AcercaDe.java
@@ -1,9 +1,8 @@
 package com.isunican.gastoporviajero;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class AcercaDe extends AppCompatActivity {
 
